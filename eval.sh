d8c8a95054f2527d4a9da91cc8fbba22ccd5f030
Rafael Lopes <rafael84@gmail.com>
2026-01-15 09:39:54 -0300

    eval output in green

--- eval.sh
+++ eval.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 set -eu
+
+printf '\033[0;32m'
+
 bin/tiny16-asm "$1" bin/eval.tiny16
-bin/tiny16-emu bin/eval.tiny16 -d -t -m "${2:-100}"
+bin/tiny16-emu bin/eval.tiny16 -d -t -m "${2:-1000}"
+
+printf '\033[0m'
