12a75afd2d74bdfcbaded942301435644c4de360
Rafael Lopes <rafael84@gmail.com>
2026-01-25 13:02:56 -0300

    tutorial: review and solve no.43

--- tutorial/43_rotate_left.asm
+++ tutorial/43_rotate_left.asm
@@ -4,7 +4,11 @@
 ; Goal: Rotate bits left (circular shift): bit 7 wraps to bit 0
 ;       Value: 0x81 (10000001) rotate left → 0x03 (00000011)
 ;
-; Instructions to use: LOADI, SHL, ADD, AND, OR, HALT
+; Note: Tiny16 immediates can only be used with LOADI (and with LOAD/STORE
+;       address operands). For bit masks like 0x80/0x01, LOADI them into a
+;       register first, then AND/OR with that register.
+;
+; Instructions to use: LOADI, MOV, SHL, AND, OR, JNC, HALT
 ; Expected result: R0 = 0x03
 ;
 ; Hint: Rotate left = shift left + wrap bit 7 to bit 0
@@ -14,9 +18,17 @@
 
 section .code
 
-; TODO: Load 0x81 into R0 (10000001)
-; TODO: Copy R0 to R1 (save for bit 7 test)
-; TODO: AND R1 with 0x80 (test bit 7)
-; TODO: SHL R0 (shift left: 10000001 → 00000010)
-; TODO: If bit 7 was set (R1 != 0), OR R0 with 0x01 (set bit 0)
-; TODO: Halt (R0 should be 0x03 = 00000011)
+loadi   r0, 0b10000001  ; Load 0x81 into R0 (10000001)
+mov     r1, r0          ; Copy R0 to R1 (save for bit 7 test)
+
+loadi   r2, 0x80
+and     r1, r2          ; AND R1 with 0x80 (test bit 7)
+
+shl     r0              ; SHL R0 (shift left: 10000001 → 00000010) (1 goes into C)
+jnc     done            ; If bit 7 was not set (C=0), skip wrapping bit 0
+
+loadi   r3, 0x01        ; Load bit-0 mask (0x01) into R3
+or      r0, r3          ; Set bit 0 (wrap): R0 |= 0x01
+
+done:
+    halt                ; Halt (R0 should be 0x03 = 00000011)
