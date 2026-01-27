de6424d79cd6cace7e58a49abfc2d2d5bececbe1
Rafael Lopes <rafael84@gmail.com>
2026-01-26 21:49:05 -0300

    tutorials: review no. 46

--- tutorial/46_sections_and_org.asm
+++ tutorial/46_sections_and_org.asm
@@ -1,22 +1,38 @@
 ; Tutorial 46: Sections and ORG
 ; Level: 7 - Assembler Power Features
 ;
-; Goal: Place data at a specific address with ORG and read it back
-; Expected result: R0 = 0x2A
+; Goal: Read array at 0x4020, compute sum, store at 0x4100
+; Expected: Memory[0x4100] = 0x96 (150 = sum of 10,20,30,40,50)
 ;
-; Hint: ORG is only valid in the .data section
-; Hint: Use expressions to split 16-bit addresses into high/low bytes
+; Concepts: Multiple ORG directives, expressions in immediates
 
-DATA_ADDR = 0x4010
+ARRAY_ADDR  = 0x4020
+RESULT_ADDR = 0x4100
+ARRAY_LEN   = 5
 
 section .code
 
-loadi r6, DATA_ADDR >> 8
-loadi r7, DATA_ADDR & 0xFF
-load  r0, [r6:r7]
+loadi   r6, ARRAY_ADDR >> 8
+loadi   r7, ARRAY_ADDR & 0xFF
+loadi   r2, ARRAY_LEN
+
+xor     r0, r0
+loop:
+    load    r1, [r6:r7]+
+    add     r0, r1
+    dec     r2
+    jnz     loop
+
+loadi   r6, RESULT_ADDR >> 8
+loadi   r7, RESULT_ADDR & 0xFF
+store   r0, [r6:r7]
+
 halt
 
 section .data
 
-org DATA_ADDR
-value: db 0x2A
+org ARRAY_ADDR
+array:  db 10, 20, 30, 40, 50
+
+org RESULT_ADDR
+result: db 0
