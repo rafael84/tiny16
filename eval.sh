7b2f72b1fa625b489497fb04449c6a9bbe235f52
Rafael Lopes <rafael84@gmail.com>
2026-01-31 12:43:12 -0300

    evals.sh: add support to SE

--- eval.sh
+++ eval.sh
@@ -1,9 +1,28 @@
 #!/bin/sh
 set -eu
 
+INPUT="$1"
+EXT="${INPUT##*.}"
+
 printf '\033[0;32m'
 
-bin/tiny16-asm "$1" bin/eval.tiny16
-bin/tiny16-emu bin/eval.tiny16 -d -t -m "${2:-1000}"
+case "$EXT" in
+    se)
+        # Compile .se to .asm first, then assemble
+        build/tiny16-sec "$INPUT" build/eval.asm
+        build/tiny16-asm build/eval.asm build/eval.tiny16
+        ;;
+    asm)
+        # Assemble directly
+        build/tiny16-asm "$INPUT" build/eval.tiny16
+        ;;
+    *)
+        printf '\033[0;31mError: Unknown file extension .%s (expected .se or .asm)\033[0m\n' "$EXT" >&2
+        exit 1
+        ;;
+esac
+
+#build/tiny16-emu build/eval.tiny16 -d -t -m "${2:-1000}"
+build/tiny16-emu build/eval.tiny16
 
 printf '\033[0m'
